b2789ff4b1e598ac917b2e1624e37a19ad5831b6
Alexander Schelchere <ascellie@gmail.com>
2018-06-20 16:04:43 -0400

    script.js: scores are being taken and wigs are getting snatched

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 let canvas;
 let canvasContext;
+
 // X & Y axis of the ball.
 let ballX = 50;
 let ballSpeedX = 15;
 let ballY = 10;
 let ballSpeedY = 4;
+
 // The paddles.
 const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
 let paddle1Y = 250;
 let paddle2Y = 250;
+
+// The players.
+let player1Score = 0;
+let player2Score = 0;
 
 // Identify the location of the user's mouse.
 // This event listener fires every time the mouse is moved.
@@ -25,7 +31,7 @@ calculateMousePos = evt => {
 };
 
 window.onload = () => {
-  console.log("Within Your Browser The Force Has Awakend.");
+  console.log("Within Your Browser, The Force Has Awakened...");
   // Grab the canvas we will be painting.
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
@@ -42,7 +48,7 @@ window.onload = () => {
     let mousePos = calculateMousePos(evt);
     paddle1Y = mousePos.y - PADDLE_HEIGHT / 2;
   });
-};
+}; // <~ wondow.onload(): closed.
 
 // Reset the position of the ball to the center of the screen.
 ballReset = () => {
@@ -51,16 +57,30 @@ ballReset = () => {
   ballX = canvas.height / 2;
 };
 
+// This will give the second paddle _life_, hunty!
+let computerMovement = () => {
+  // The center of the second paddle.
+  let paddle2YCenter = paddle2Y + PADDLE_HEIGHT / 2;
+  if (paddle2YCenter < ballY - 35) {
+    paddle2Y += 6;
+  } else if (paddle2YCenter > ballY + 35) {
+    paddle2Y -= 6;
+  }
+};
+
 // This handles all the movement/functionality of the items in our canvas.
 moveEverything = () => {
+  computerMovement();
+
   ballX += ballSpeedX;
   ballY += ballSpeedY;
+
   if (ballX < 0) {
-    // ballSpeedX = -ballSpeedX;
     if (ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT) {
       ballSpeedX = -ballSpeedX;
     } else {
       ballReset();
+      player2Score++;
     }
   }
   if (ballX > canvas.width) {
@@ -68,6 +88,7 @@ moveEverything = () => {
       ballSpeedX = -ballSpeedX;
     } else {
       ballReset();
+      player1Score++;
     }
   }
   if (ballY < 0) {
@@ -76,7 +97,8 @@ moveEverything = () => {
   if (ballY > canvas.height) {
     ballSpeedY = -ballSpeedY;
   }
-};
+}; // <~ Move Everything Function: closed.
+
 // This draws all the shapes/items on our canvas.
 drawEverything = () => {
   // Background of our canvas.
@@ -93,7 +115,11 @@ drawEverything = () => {
   );
   // Draws the ball.
   colorCircle(ballX, ballY, 10, "#FFF");
-};
+
+  // Display Scores!!
+  canvasContext.fillText("player: " + player1Score, 50, 50);
+  canvasContext.fillText("computer: " + player2Score, canvas.width - 100, 50);
+}; // <~ Draw Everything Function: closed.
 
 // This makes it easier for us draw a ball.
 colorCircle = (centerX, centerY, radius, drawColor) => {
@@ -102,6 +128,7 @@ colorCircle = (centerX, centerY, radius, drawColor) => {
   canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
   canvasContext.fill();
 };
+
 // This will simplify our drawEverything function.
 colorRect = (leftX, topY, width, height, drawColor) => {
   canvasContext.fillStyle = drawColor;
