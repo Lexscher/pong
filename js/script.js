b09e7484cba131cb5f4b1cdc3fab459dd989ecb1
Alexander Schelchere <ascellie@gmail.com>
2018-06-20 11:12:48 -0400

    Added script to modify the canvas.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-window.onload = () => {
-  console.log("Hello World!");
-};
+console.log("Hello World!");
+
+// Grab the canvas we will be painting
+let canvas = document.getElementById("gameCanvas");
+let canvasContext = canvas.getContext("2d");
+canvasContext.fillStyle = "#000";
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
